9ea3e4b60173d6d073bdfa2ec73873bfb9531e52
Hunar Roop Kahlon <hunar.roop@gmail.com>
2020-10-05 20:21:18 -0700

    add forbid rules
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! Java Language Specification compliant parser, written in Rust
+
+#![forbid(
+    missing_copy_implementations,
+    missing_crate_level_docs,
+    missing_debug_implementations,
+    missing_doc_code_examples,
+    missing_docs,
+)]
